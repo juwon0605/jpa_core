ed6f820a2f240a6c7b8ed24bdc2ff1096eaddeef
Juwon Park <devprofessionalism@gmail.com>
2023-01-21 17:25:58 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,22 @@ import lombok.Getter;
 
 @Embeddable
 @Getter
+// @Setter // 값 타입은 변경이 불가능하게 막아야 좋음
 public class Address {
 
 	private String city;
 	private String street;
 	private String zipcode;
+
+	//값 타입이니 빈 생성자를 public하게 두지 말기
+	//그러나 JPA에서 리플렉션이나 다이나믹 프록시 사용하려면 참조할 수 있어야함
+	//JPA 스펙상 protected까지 허용가능하니 protected 권장
+	protected Address() {
+	}
+
+	public Address(String city, String street, String zipcode) {
+		this.city = city;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
 }
---
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "category_id")
+	private Long id;
+
+	private String name;
+
+	@ManyToMany // 실무에서 사용하지는 않는다. 단순 다대다 양방향 관계 예시
+	@JoinTable(name = "category_item", // 실제 데이터베이스는 다대다 관계가 없어서 테이블로 매핑해야 한다
+		joinColumns = @JoinColumn(name = "category_id"),
+		inverseJoinColumns = @JoinColumn(name = "item_id"))
+	private List<Item> items = new ArrayList<>();
+
+	@ManyToOne
+	@JoinColumn(name = "parent_id")
+	private Category parent;
+
+	@OneToMany(mappedBy = "parent")
+	private List<Category> child = new ArrayList<>();
+}
