a5ec4624476f1a5404079cb2b6ba357ef94ed131
Juwon Park <devprofessionalism@gmail.com>
2023-01-22 17:05:36 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import javax.persistence.InheritanceType;
 import javax.persistence.ManyToMany;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -34,4 +35,25 @@ public abstract class Item {
 
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+
+	//==비즈니스 로직==//
+	//도메인 객체가 자신의 변수에 관련된 함수를 가지고 있는 것이 좀 더 응집도가 높고 객체지향적이다
+
+	/**
+	 * stock 증가
+	 */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stock 감소
+	 */
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if (restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}
+		this.stockQuantity = restStock;
+	}
 }
