d899d82bc1bf95bd5a198dacf5873854852f4e91
박주원 <devprofessionalism@gmail.com>
2023-02-15 12:41:54 +0900

    회원 등록 API(~10:00)

---
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.api;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+	private final MemberService memberService;
+
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class CreateMemberResponse {
+		private Long id;
+	}
+
+}
