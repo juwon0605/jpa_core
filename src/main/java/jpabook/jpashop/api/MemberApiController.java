147ea92ad6686c68724ca7a49696c4a29903b15d
박주원 <devprofessionalism@gmail.com>
2023-02-16 11:36:44 +0900

    회원 수정 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -3,7 +3,9 @@ package jpabook.jpashop.api;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,29 @@ public class MemberApiController {
 
 		Long id = memberService.join(member);
 		return new CreateMemberResponse(id);
+	}
+
+	@PutMapping("/api/v2/members/{id}")
+	public UpdateMemberResponse updateMemberV2(
+		@PathVariable("id") Long id,
+		@RequestBody @Valid UpdateMemberRequest request
+	) {
+		memberService.update(id, request.getName());
+		Member findMember = memberService.findOne(id);
+		return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+	}
+
+	@Data
+	@AllArgsConstructor
+	static class UpdateMemberResponse {
+		private Long id;
+		private String name;
+	}
+
+	@Data
+	static class UpdateMemberRequest {
+		@NotEmpty
+		private String name;
 	}
 
 	@Data
