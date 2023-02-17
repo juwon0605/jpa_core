5ecddf47124c41af36ba099136a2ee004f47910f
박주원 <devprofessionalism@gmail.com>
2023-02-17 10:09:31 +0900

    간단한 주문 조회 V3: 엔티티를 DTO로 변환 - 페치 조인 최적화

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -101,4 +101,11 @@ public class OrderRepository {
 			.setMaxResults(1000); //최대 1000 건
 		return query.getResultList();
 	}
+
+	public List<Order> findAllWithMemberDelivery() {
+		return em.createQuery("select o from Order o "
+				+ "					join fetch o.member m "
+				+ "					join fetch o.delivery d", Order.class)
+			.getResultList();
+	}
 }
