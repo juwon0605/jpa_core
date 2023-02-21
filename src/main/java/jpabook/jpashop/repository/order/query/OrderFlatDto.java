c6e8c13ad1e4444eebab115a247bfe5520cf63d6
Juwon Park <devprofessionalism@gmail.com>
2023-02-21 09:08:58 +0900

    주문 조회 V6: JPA에서 DTO로 직접 조회, 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderFlatDto.java
@@ -1,52 +1,41 @@
 package jpabook.jpashop.repository.order.query;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.OrderStatus;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 
 @Data
-@EqualsAndHashCode(of = "orderId")
-public class OrderQueryDto {
+public class OrderFlatDto {
 
 	private Long orderId;
 	private String name;
 	private LocalDateTime orderDate;
 	private OrderStatus orderStatus;
 	private Address address;
-	private List<OrderItemQueryDto> orderItems;
 
-	public OrderQueryDto(
-		Long orderId,
-		String name,
-		LocalDateTime orderDate,
-		OrderStatus orderStatus,
-		Address address
-	) {
-		this.orderId = orderId;
-		this.name = name;
-		this.orderDate = orderDate;
-		this.orderStatus = orderStatus;
-		this.address = address;
-	}
+	private String itemName;
+	private int orderPrice;
+	private int count;
 
-	public OrderQueryDto(
+	public OrderFlatDto(
 		Long orderId,
 		String name,
 		LocalDateTime orderDate,
 		OrderStatus orderStatus,
 		Address address,
-		List<OrderItemQueryDto> orderItems
+		String itemName,
+		int orderPrice,
+		int count
 	) {
 		this.orderId = orderId;
 		this.name = name;
 		this.orderDate = orderDate;
 		this.orderStatus = orderStatus;
 		this.address = address;
-		this.orderItems = orderItems;
+		this.itemName = itemName;
+		this.orderPrice = orderPrice;
+		this.count = count;
 	}
-
 }
